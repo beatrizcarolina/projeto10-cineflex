309b421783c9236ad377bac58ce061bda0b2d044
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-05-27 19:15:50 -0300

    fix export const

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components"
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
 	button {
 		height: 43px;
 		background: #E8833A;
@@ -37,4 +37,3 @@ const GlobalStyle = createGlobalStyle`
 	}
 `
 
-export default GlobalStyle
