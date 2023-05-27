283853916d2ba1e9d2735ec4146d5f8da605c34b
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-05-27 19:19:48 -0300

    create component Movies from HomePage

---
+++ src/components/Movies.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default function Movies({movie}) {
+    
+    return (
+        <MovieContainer>
+        <Link to={`/sessoes/${movie.id}`}>
+            <img src={movie.posterURL} alt={movie.title} />
+        </Link>
+    </MovieContainer>  
+    )
+}
+
+const MovieContainer = styled.div`
+    width: 145px;
+    height: 210px;
+    box-shadow: 0px 2px 4px 2px #0000001A;
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    img {
+        width: 130px;
+        height: 190px;
+    }
+`
